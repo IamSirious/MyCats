76bb320947c84220b6bafca7a4bda448d9778ff1
IamSirious <sirgey.romanenko@yandex.ru>
2023-04-15 22:55:04 +0300

    Корректировка проекта

--- js/global.js
+++ js/global.js
@@ -1,6 +1,6 @@
 const block = document.querySelector(".wrapper");
 const addBtn = document.querySelector(".add");
-const mdBox =document.querySelector (".modal-block");
+const mdBox = document.querySelector(".modal-block");
 const mdClose = mdBox.querySelector(".modal-close");
 // const mdClose = mdBox.firstElementChild;
 const addForm = document.forms.add;
@@ -18,14 +18,14 @@ let path = `https://cats.petiteweb.dev/api/single/${name}`;
 let pets = localStorage.getItem("band-cats");
 // let pets = localStorage.getItem("unic");
 if (pets) {
-    try {
-        pets = JSON.parse(pets);
-        for (let pet of pets) {
-            createCard(pet, block);
-        }
-    } catch(err) {
-        console.warn(err.message);
-        pets = null;
+  try {
+    pets = JSON.parse(pets);
+    for (let pet of pets) {
+      createCard(pet, block);
     }
+  } catch (err) {
+    console.warn(err.message);
+    pets = null;
+  }
 }
 console.log("pets", pets);
